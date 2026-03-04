e90e35bb82050b63e7e7e5881bfea5c762dfb3b1
Asura <asurahurbanova@gmail.com>
2026-03-04 01:00:03 +0100

    fix js form 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,14 +2,10 @@ const formData = {
   email: '',
   message: '',
 };
+
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', event => {
-  const { name, value } = event.target;
-  formData[name] = value.trim(); // сохраняем данные без пробелов
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData)); // сохраняем в localStorage
-});
-document.addEventListener('DOMContentLoaded', () => {
+function initializeForm() {
   const savedData = localStorage.getItem('feedback-form-state');
   if (savedData) {
     const parsedData = JSON.parse(savedData);
@@ -19,19 +15,28 @@ document.addEventListener('DOMContentLoaded', () => {
     form.elements.email.value = formData.email;
     form.elements.message.value = formData.message;
   }
-});
-form.addEventListener('submit', event => {
-  event.preventDefault();
 
-  if (!formData.email || !formData.message) {
-    alert('Fill please all fields');
-    return;
-  }
+  form.addEventListener('input', event => {
+    const { name, value } = event.target;
+    formData[name] = value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  });
 
-  console.log(formData);
+  form.addEventListener('submit', event => {
+    event.preventDefault();
 
-  form.reset();
-  localStorage.removeItem('feedback-form-state');
-  formData.email = '';
-  formData.message = '';
-});
+    if (!formData.email || !formData.message) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    console.log(formData);
+
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+    formData.email = '';
+    formData.message = '';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', initializeForm);
